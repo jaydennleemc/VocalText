163bbf767d7cfcedaf736edef27aeee651392c45
Jayden Lee <jaydenmclee@mtr.com.hk>
2025-09-16 09:41:45 +0800

    Initial Commit

---
+++ VocalText/VocalTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocalTextApp.swift
+//  VocalText
+//
+//  Created by LEEJAYMC on 16/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct VocalTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
